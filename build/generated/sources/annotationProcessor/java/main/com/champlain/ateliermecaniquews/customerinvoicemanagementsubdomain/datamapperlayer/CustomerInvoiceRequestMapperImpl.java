00acc152599f20871d196d4bcab5d210f0e93947
MichaelVacca <michaelvacca07@gmail.com>
2024-02-15 02:55:21 -0500

    Specify Java 17 runtime

--- build/generated/sources/annotationProcessor/java/main/com/champlain/ateliermecaniquews/customerinvoicemanagementsubdomain/datamapperlayer/CustomerInvoiceRequestMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/champlain/ateliermecaniquews/customerinvoicemanagementsubdomain/datamapperlayer/CustomerInvoiceRequestMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-14T23:55:37-0500",
+    date = "2024-02-15T02:19:39-0500",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.4.jar, environment: Java 17.0.6 (Oracle Corporation)"
 )
 @Component
